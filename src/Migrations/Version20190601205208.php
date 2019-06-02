ff1491b20fb65d178cfca4bb4c55aa8c1fcc898a
smouheb <smael.mouheb@gmail.com>
2019-06-02 12:04:49 +0200

    Added migratoin related to user table

--- src/Migrations/Version20190601205208.php
+++ src/Migrations/Version20190601205208.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190511103140 extends AbstractMigration
+final class Version20190601205208 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,10 +22,7 @@ final class Version20190511103140 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE address DROP FOREIGN KEY FK_D4E6F818BAC62AF');
-        $this->addSql('DROP TABLE city');
-        $this->addSql('DROP INDEX IDX_D4E6F818BAC62AF ON address');
-        $this->addSql('ALTER TABLE address ADD city VARCHAR(255) NOT NULL, CHANGE city_id country_id INT DEFAULT NULL');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE address ADD CONSTRAINT FK_D4E6F81F92F3E70 FOREIGN KEY (country_id) REFERENCES country (id)');
         $this->addSql('CREATE INDEX IDX_D4E6F81F92F3E70 ON address (country_id)');
     }
@@ -35,12 +32,8 @@ final class Version20190511103140 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE city (id INT AUTO_INCREMENT NOT NULL, country_id INT DEFAULT NULL, cityname VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci, created_at DATETIME NOT NULL, updated_at DATETIME DEFAULT NULL, INDEX IDX_2D5B0234F92F3E70 (country_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE city ADD CONSTRAINT FK_2D5B0234F92F3E70 FOREIGN KEY (country_id) REFERENCES country (id)');
+        $this->addSql('DROP TABLE user');
         $this->addSql('ALTER TABLE address DROP FOREIGN KEY FK_D4E6F81F92F3E70');
         $this->addSql('DROP INDEX IDX_D4E6F81F92F3E70 ON address');
-        $this->addSql('ALTER TABLE address DROP city, CHANGE country_id city_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE address ADD CONSTRAINT FK_D4E6F818BAC62AF FOREIGN KEY (city_id) REFERENCES city (id)');
-        $this->addSql('CREATE INDEX IDX_D4E6F818BAC62AF ON address (city_id)');
     }
 }
