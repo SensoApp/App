7bf3a3b1426cadf756b30e815b16616a72b38f92
smouheb <smael.mouheb@gmail.com>
2019-07-07 21:13:11 +0200

    creating Timesheet, first entity created

---
+++ src/Entity/Timesheet.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TimesheetRepository")
+ */
+class Timesheet
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbreDaysWorked;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbreDaysOff;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbreOfSaturdays;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbreOfSundays;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $userId;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $status;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNbreDaysWorked(): ?int
+    {
+        return $this->nbreDaysWorked;
+    }
+
+    public function setNbreDaysWorked(int $nbreDaysWorked): self
+    {
+        $this->nbreDaysWorked = $nbreDaysWorked;
+
+        return $this;
+    }
+
+    public function getNbreDaysOff(): ?int
+    {
+        return $this->nbreDaysOff;
+    }
+
+    public function setNbreDaysOff(int $nbreDaysOff): self
+    {
+        $this->nbreDaysOff = $nbreDaysOff;
+
+        return $this;
+    }
+
+    public function getNbreOfSaturdays(): ?int
+    {
+        return $this->nbreOfSaturdays;
+    }
+
+    public function setNbreOfSaturdays(int $nbreOfSaturdays): self
+    {
+        $this->nbreOfSaturdays = $nbreOfSaturdays;
+
+        return $this;
+    }
+
+    public function getNbreOfSundays(): ?int
+    {
+        return $this->nbreOfSundays;
+    }
+
+    public function setNbreOfSundays(int $nbreOfSundays): self
+    {
+        $this->nbreOfSundays = $nbreOfSundays;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->userId;
+    }
+
+    public function setUserId(int $userId): self
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
---
+++ src/Migrations/Version20190707191105.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190707191105 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE timesheet (id INT AUTO_INCREMENT NOT NULL, nbre_days_worked INT NOT NULL, nbre_days_off INT NOT NULL, nbre_of_saturdays INT NOT NULL, nbre_of_sundays INT NOT NULL, date DATE NOT NULL, user_id INT NOT NULL, status VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE timesheet');
+    }
+}
